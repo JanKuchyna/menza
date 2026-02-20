c39d80369611b3e8ae305c80fd4cc66d8c50def6
t_baier <t_baier@utb.cz>
2026-02-20 14:31:42 +0100

    Přidejte soubory projektu.

---
+++ UTB.Library.AppHost/AppHost.cs
@@ -0,0 +1,15 @@
+var builder = DistributedApplication.CreateBuilder(args);
+
+var postgres = builder.AddPostgres("postgres")
+                      .WithPgAdmin(c => c.WithLifetime(ContainerLifetime.Persistent))
+                      .WithDataVolume()
+                      .WithLifetime(ContainerLifetime.Persistent);
+
+var database = postgres.AddDatabase("database");
+
+builder.AddProject<Projects.UTB_Library_DbManager>("utb-library-dbmanager")
+       .WithReference(database)
+       .WithHttpCommand("reset-db", "Reset Database")
+       .WaitFor(database); 
+
+builder.Build().Run();
